0bff1656912e957d34b42a5f0966767aebd1363e
iouhase <emilowithc@gmail.com>
2025-05-11 16:19:15 +0200

    html and other stuff :)

--- src/main/java/net/iouhase/gilbert/application/RepositoryConfig.java
+++ src/main/java/net/iouhase/gilbert/application/RepositoryConfig.java
@@ -1,9 +1,20 @@
 package net.iouhase.gilbert.application;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import javax.sql.DataSource;
 
 @Configuration
 public class RepositoryConfig {
 
+    @Bean
+    public DataSource mysqlDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl("jdbc:mysql://iouhase.net:3306/gilbert");
+        dataSource.setUsername("user");
+        dataSource.setPassword("26Uj96MSlPMV4o3aHqIypWcu");
 
+        return dataSource;
+    }
 }
