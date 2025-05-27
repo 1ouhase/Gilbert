b4f8205edcae5c4bb72e363551d4198629b8b58e
iouhase <emilowithc@gmail.com>
2025-05-27 20:35:06 +0200

    when the ting tong

--- src/main/java/net/iouhase/gilbert/controller/ProductController.java
+++ src/main/java/net/iouhase/gilbert/controller/ProductController.java
@@ -1,8 +1,12 @@
 package net.iouhase.gilbert.controller;
 
+import net.iouhase.gilbert.model.Product;
 import net.iouhase.gilbert.usecase.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ProductController {
@@ -13,8 +17,20 @@ public class ProductController {
     }
 
     @GetMapping("/createPost")
-    public String advertisement() {
+    public String getCreatePost() {
         return "createPost";
+    }
+
+    @PostMapping("/createPost")
+    public String createPost(@RequestParam String name, @RequestParam String category, @RequestParam double price, @RequestParam double size, @RequestParam String picture) {
+        Product product = new Product();
+        product.setCategory(category);
+        product.setModel(name);
+        product.setPrice(price);
+        product.setSize((int)size);
+        product.setImage(picture);
+        productService.save(product);
+        return "redirect:/profile";
     }
 
     @GetMapping("/search")
